054c366b2df01917b7d29d6e231a7a421d7ca576
Imran A <74223481+CubeWolv@users.noreply.github.com>
2022-12-23 21:38:43 -1200

    delete

--- appals/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('privacypolicy/', views.privacypolicy, name='privacypolicy'),
-    path('aboutus/', views.about, name='about'),
-]
-
--- appals/blognews/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BlognewsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blognews'
